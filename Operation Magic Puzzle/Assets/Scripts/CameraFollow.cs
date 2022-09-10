153603a90b6e3555adcab9371a4afbd092a9a4b7
Rachel Quijano <71981920+rachelquijano@users.noreply.github.com>
2022-09-10 11:55:36 -0400

    Main Menu Camera

--- Operation Magic Puzzle/Assets/Scripts/CameraFollow.cs
+++ Operation Magic Puzzle/Assets/Scripts/CameraFollow.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     private Transform target;
-    private float followSpeed = 1f;
+    private float followSpeed = 3f;
     private bool canFollow = false;
+    private Animator anim;
 
     private void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        anim = GetComponent<Animator>();
 
     }
     private void Update()
     {
+
         if (canFollow)
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, -10f), followSpeed * Time.deltaTime);
     }
@@ -22,5 +25,7 @@ public class CameraFollow : MonoBehaviour
     public void setCanFollow(bool boolState)
     {
         canFollow = boolState;
+        anim.enabled = false;
+        
     }
 }
