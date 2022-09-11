36d1abc6cc51bd89a04f70ef43cfa6e779c3a2ec
Rachel Quijano <71981920+rachelquijano@users.noreply.github.com>
2022-09-10 22:50:46 -0400

    Fixed camera jag and clouds.

--- Operation Magic Puzzle/Assets/Scripts/CameraFollow.cs
+++ Operation Magic Puzzle/Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,7 @@ public class CameraFollow : MonoBehaviour
     private Transform target;
     private float followSpeed = 5f;
     [SerializeField] bool canFollow = false;
+    private bool animateToPlayer;
     private Animator anim;
 
     private void Start()
@@ -19,13 +20,24 @@ public class CameraFollow : MonoBehaviour
     {
 
         if (canFollow)
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10f);
+
+        if (animateToPlayer)
+        {
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, -10f), followSpeed * Time.deltaTime);
+        }
+
+        if(Vector3.Distance(target.transform.position, gameObject.transform.position) < 0.1f)
+        {
+            animateToPlayer = false;
+        }
     }
 
-    public void setCanFollow(bool boolState)
+    public IEnumerator CanFollow()
     {
-        canFollow = boolState;
+        animateToPlayer = true;
         anim.enabled = false;
-        
+        yield return new WaitForSeconds(2f);
+        canFollow = true;
     }
 }
